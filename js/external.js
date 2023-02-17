a5e4872c74db771cee2488323de07269ef9634d1
Alex Perez <alex.f.perez92@gmail.com>
2023-02-17 12:24:23 -0600

    js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("this is my external js file: yo");
+let quantity = 3;
+console.log(quantity);
+
+//alert("Have a good lunch!");
+
+let submit = confirm("Are you sure you want to resubmit this form?");
+console.log(submit);
+
+let age = prompt( "What is your age?")
+alert("Wow, your old. You're " + age + "!");

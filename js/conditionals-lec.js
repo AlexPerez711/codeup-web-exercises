e731d3e3f2e2ff34f806f9f99a07e3c54f42c68f
Alex Perez <alex.f.perez92@gmail.com>
2023-02-22 12:12:32 -0600

    conditional exercise

---
+++ js/conditionals-lec.js
@@ -0,0 +1,79 @@
+// console.log("testing")
+//
+// let age = 19;
+//
+// if (age >= 16) {
+//     console.log("Old enough to drive.")
+// }else if (age > 0 && age < 16)
+// {
+//     console.log("Not old enough to drive.")
+// }else {
+//     console.log("Not a valid age.")
+// }
+
+//if/els if/else ex:
+
+// let daysOfTheWeek = prompt("Enter the day of the week")
+// if (daysOfTheWeek === "Monday"){
+//     console.log("I hate " + daysOfTheWeek)
+// }else if (daysOfTheWeek === "Tuesday"){
+//     console.log( "Tuesdays are better")
+// }else {
+//     console.log("Not a valid day of the week")
+// }
+// if (daysOfTheWeek === "Monday" || daysOfTheWeek === "Tuesday" || daysOfTheWeek === "Wednesday"){
+//     console.log(daysOfTheWeek + "is my favorite day")
+// }else{
+//     console.log("Not a valid day of the week")
+// }
+//
+// let num = 42;
+//
+//
+// if (typeof num === "number"){
+//     console.log("typeof ' num' is a number")
+// } else {
+//     console.log("typeof is not a number")
+// // }
+//
+// let username = "user";
+// // let password = "password";
+//
+// // let userUsername = prompt("Enter your username.");
+// // let userPassword = prompt("Enter your password.");
+//
+// // if(username === userUsername && password === userPassword){
+// //     console.log("Successfully logged in")
+// // }else if (username !== userUsername && password !== userPassword){
+// //     console.log("Invalid username and password.")
+// // }else if (username !== userUsername){
+// //     console.log("Invalid username.")
+// // }else if (password !== userPassword){
+// //     console.log("Invalid password.")
+// // }
+//
+// // password >= 6 characters
+// // password must not include a space
+//
+// let password = prompt("Please enter a password");
+//
+// if(password.length >= 6){
+//     console.log("Valid password")
+// }else{
+//     console.log("Password does not meet the requirements.")
+// }
+
+
+//Ternary operations (only for if/else statements not for if/else/if/else)
+// ? means if ....... : means else
+//
+// let name = "tim"
+// let greeting = name === "bob" ? "Hello " + name : "hello stranger";
+// console.log(greeting)
+//
+// same as
+//if(name === bob){
+// console.log("hello " = name)
+//}else{
+//console.log("hello stranger")
+//}

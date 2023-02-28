059b85a7237cfb709d740f0a552b9c827c1f4335
Alex Perez <alex.f.perez92@gmail.com>
2023-02-28 09:47:26 -0600

    corrections

--- js/planets-string.js
+++ js/planets-string.js
@@ -17,13 +17,24 @@
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
+     */
+    /**
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-let planetstr = planetsArray.join('\n');
+let planetstr = planetsArray.join("<br>");
     console.log(planetstr);
 
+
+
+    let ulPlanets = "<ul>";
+    let liString;
+    planetsArray.forEach(function (planet){
+        liString= "<li>" + planet + "</li>";
+        ulPlanets += liString
+    })
+    ulPlanets += "</ul";
+    console.log(ulPlanets)
 })();
